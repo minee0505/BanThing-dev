e33a29b13a9e5923166588ef0cebbc5a69c25228
songkey06 <mjsong0524@gmail.com>
2025-09-12 16:38:36 +0900

    refactor: [BE] JPA 레포지토리 명명 규칙 수정 (#110)
    - JPA 레포지토리 클래스의 대소문자 수정 (PascalCase 준수)

--- backend/src/main/java/com/nathing/banthing/repository/ChatbotConversationsRepository.java
+++ backend/src/main/java/com/nathing/banthing/repository/ChatbotConversationsRepository.java
@@ -3,5 +3,5 @@ package com.nathing.banthing.repository;
 import com.nathing.banthing.entity.ChatbotConversation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface chatbotConversationsRepository extends JpaRepository<ChatbotConversation, Long> {
+public interface ChatbotConversationsRepository extends JpaRepository<ChatbotConversation, Long> {
 }
