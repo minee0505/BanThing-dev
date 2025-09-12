dcf10f45f47e55e0c717a1b075b8e722d321bd97
songkey06 <mjsong0524@gmail.com>
2025-09-12 20:08:58 +0900

    feat: [BE] 피드백 생성 로직 구현 및 관련 클래스 추가 (#120)
    - FeedbackService에 피드백 생성 메서드(createFeedback) 추가 및 유효성 검사 로직 구현
    - FeedbackCreateRequest DTO 추가
    - ScoreEvent Enum 추가 및 점수 이벤트 정의
    - Feedback 엔티티의 getScoreValue 메서드 제거
    - JPA Repository 인터페이스 명명 수정 (PascalCase 준수)

--- backend/src/main/java/com/nathing/banthing/repository/UsersRepository.java
+++ backend/src/main/java/com/nathing/banthing/repository/UsersRepository.java
@@ -3,5 +3,5 @@ package com.nathing.banthing.repository;
 import com.nathing.banthing.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface usersRepository extends JpaRepository<User, Long> {
+public interface UsersRepository extends JpaRepository<User, Long> {
 }
