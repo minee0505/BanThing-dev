4e63e94cc9ca2368da3ec701eb1485f8c695b92c
rhehdgus8831 <rhehdgus8831@naver.com>
2025-09-12 17:35:42 +0900

    refactor: [BE] Entity 클래스에 AllArgsConstructor 및 Builder 추가 (#17)
    - Meeting 및 MeetingParticipant 엔티티에 @AllArgsConstructor, @Builder 애노테이션 적용
    - 코드를 간결화하고 객체 생성 시 가독성 향상

--- backend/src/main/java/com/nathing/banthing/entity/Meeting.java
+++ backend/src/main/java/com/nathing/banthing/entity/Meeting.java
@@ -1,10 +1,7 @@
 package com.nathing.banthing.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -17,6 +14,8 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class Meeting {
 
     @Id
