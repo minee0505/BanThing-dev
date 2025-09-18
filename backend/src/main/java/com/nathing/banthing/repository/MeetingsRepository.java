93abc87362880bb9b2c5b976214f87fed264fecf
Kanggwanju <xprtktm1@gmail.com>
2025-09-18 17:30:05 +0900

    feat: [BE] 승인된 모임 페이징 조회 기능 및 테스트 코드 추가 (#64)
    - MeetingsRepository에 findApprovedMeetingsWithMartByUserId 메서드 추가
      - 네이티브 쿼리를 사용하여 특정 유저의 승인된 모임 목록을 최신순으로 조회
      - Pageable 지원을 통해 페이징 처리 구현
    - MeetingsRepositoryTest에 관련 테스트 코드 작성
      - 테스트 데이터 기반으로 조회 결과 검증 및 출력

--- backend/src/main/java/com/nathing/banthing/repository/MeetingsRepository.java
+++ backend/src/main/java/com/nathing/banthing/repository/MeetingsRepository.java
@@ -1,6 +1,8 @@
 package com.nathing.banthing.repository;
 
 import com.nathing.banthing.entity.Meeting;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -52,4 +54,38 @@ public interface MeetingsRepository extends JpaRepository<Meeting, Long> {
      */
     @Query("SELECT m FROM Meeting m JOIN FETCH m.mart ORDER BY m.createdAt DESC")
     List<Meeting> findAllWithMartByOrderByCreatedAtDesc();
+
+    /**
+     * 주어진 사용자 ID에 대해 승인된 모임 목록을 반환하는 메서드입니다. (최신순)
+     *
+     * 네이티브 쿼리로 JOIN을 통해
+     * Meeting과 Mart, 모임 참가자 정보를 모두 가져오고 있기 때문에
+     * N+1 문제는 발생하지 않음
+     *
+     * JPA가 Pageable 정보를 쿼리에 자동으로 반영해서 LIMIT와 OFFSET을 추가,
+     * countQuery를 통해 전체 개수도 별도로 가져와서
+     * Page 객체에 필요한 모든 정보를 한 번에 얻을 수 있음
+     *
+     * @param userId 사용자 ID
+     * @param pageable 페이징 정보를 포함하는 Pageable 객체
+     * @return 승인된 모임 목록을 담고 있는 Page 객체
+     */
+    @Query(value = """
+        SELECT m.*
+        FROM meetings m
+        JOIN meeting_participants mp ON m.meeting_id = mp.meeting_id
+        JOIN marts ma ON m.mart_id = ma.mart_id
+        WHERE mp.user_id = :userId AND mp.application_status = 'APPROVED'
+            AND m.deleted_at IS NULL
+        ORDER BY m.created_at DESC
+    """,
+    countQuery = """
+        SELECT count(m.meeting_id)
+        FROM meetings m
+        JOIN meeting_participants mp ON m.meeting_id = mp.meeting_id
+        WHERE mp.user_id = :userId AND mp.application_status = 'APPROVED'
+            AND m.deleted_at IS NULL
+    """,
+    nativeQuery = true)
+    Page<Meeting> findApprovedMeetingsWithMartByUserId(@Param("userId") Long userId, Pageable pageable);
 }
