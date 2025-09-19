a856ad0646de95d01503c5ba603b7b677de87460
Kanggwanju <xprtktm1@gmail.com>
2025-09-19 11:31:34 +0900

    feat: [FE] 프로필 페이지 사용자 정보 표시 및 로그인 검증 로직 추가 (#60)
    - MyInfo 컴포넌트에 사용자 정보 표시 기능 추가
      - 사용자 이름, 프로필 이미지, 등급, 점수, 노쇼 카운트 등을 출력
    - ProfilePage에 로그인 여부를 확인하는 useEffect 추가
      - 인증되지 않은 경우 로그인 페이지로 리다이렉트하도록 구현
    - 코드 전반에 user 상태 및 관련 데이터 활용 로직 반영

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import MyInfo from '../components/Profile/MyInfo.jsx';
 import MyParticipatingGroups from '../components/Profile/MyParticipatingGroups.jsx';
 import MyPendingGroups from '../components/Profile/MyPendingGroups.jsx';
+import {useAuthStore} from '../stores/authStore.js';
+import {useNavigate} from 'react-router-dom';
 
 const ProfilePage = () => {
+
+  const { isAuthenticated, user } = useAuthStore();
+  const navigate = useNavigate();
+
+  // 로그인 버튼 활성화 여부 검증을 위한 useEffect
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate('/login');
+    }
+  }, [isAuthenticated, navigate]);
+
+  useEffect(() => {})
+
   return (
     <>
       <div>
         <h1>프로필 페이지</h1>
-        <MyInfo />
+        <MyInfo user={user}/>
         <MyParticipatingGroups />
         <MyPendingGroups />
       </div>
