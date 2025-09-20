2f25179698e6766a676ce917553e10eec9c45203
Kanggwanju <xprtktm1@gmail.com>
2025-09-20 17:35:22 +0900

    feat: [FE] 프로필 페이지 로딩 데이터를 위한 로더 추가 (#60)
    - ProfilePage 진입 시 초기 모임 데이터 로더(profileMeetingLoader) 구현
    - ProfilePage 라우트에 loader 옵션 추가로 데이터 사전 로드 지원
    - 승인된 모임 데이터 기반으로 스켈레톤 개수 계산 로직 적용

---
+++ frontend/src/loaders/profileMeetingLoader.js
@@ -0,0 +1,18 @@
+import {profileMeetings} from "../services/profileApi.js";
+
+// 프로필 최초 진입했을때 스켈레톤 몇 개 보여줄지 계산용 로더 함수
+// 페이지에 진입하기 전에 실행됨.
+export const profileMeetingLoader = async () => {
+  // 한 페이지에 보여지는 모임 개수는 최대 4개
+  const meetingsPerPage = 4;
+
+  // 첫 페이지(page=0, APPROVED 조건)
+  const result = await profileMeetings(0, 'APPROVED');
+
+  if (result.success) {
+    const totalElements = result.data.totalElements;
+    return Math.min(meetingsPerPage, totalElements); // 첫 페이지 개수
+  } else {
+    return meetingsPerPage; // 실패하면 기본 4개 보여주기
+  }
+};
--- frontend/src/routes/router-config.jsx
+++ frontend/src/routes/router-config.jsx
@@ -11,6 +11,7 @@ import LoadMeRoute from "../components/Auth/LoadMeRoute.jsx";
 import MeetingCreatePage from "../pages/MeetingCreatePage.jsx";
 import PrivateRoute from "../components/Auth/PrivateRoute.jsx";
 import ProfilePage from "../pages/ProfilePage.jsx";
+import {profileMeetingLoader} from "../loaders/profileMeetingLoader.js";
 
 // 라우터 설정
 export const router = createBrowserRouter([
@@ -36,7 +37,7 @@ export const router = createBrowserRouter([
       { path: 'meetings/:id', element: <MeetingDetailPage /> },
       // { path: 'meetings/:id/comments', element: <CommentPage /> },
       { path: 'login', element: <LoginPage /> },
-      { path: 'profile/:id', element: <ProfilePage /> },
+      { path: 'profile/:id', element: <ProfilePage />, loader: profileMeetingLoader},
     ],
   },
   {
