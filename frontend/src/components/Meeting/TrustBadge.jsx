f9facdee10e6591798cffe006af30e5fde72d4fc
songkey06 <mjsong0524@gmail.com>
2025-09-22 10:24:39 +0900

    feat: [BE] 마이페이지에 트러스트 뱃지 추가
    -뱃지 컴포넌트 추가
    -뱃지 크기 확대

--- frontend/src/components/Meeting/TrustBadge.jsx
+++ frontend/src/components/Meeting/TrustBadge.jsx
@@ -5,11 +5,11 @@ import styles from './TrustBadge.module.scss';
 const TrustBadge = ({ trustScore }) => {
     let trustClassName = '';
 
-    if (trustScore >= 500) {
+    if (trustScore >= 500) { //500 정 이상 파랑 뱃지
         trustClassName = styles.trustGood;
-    } else if (trustScore >= 300) {
+    } else if (trustScore >= 100) {// 499 ~100 사이 초록 뱃지
         trustClassName = styles.trustBasic;
-    } else {
+    } else {                        // 99이하 빨강 뱃지
         trustClassName = styles.trustWarning;
     }
 
